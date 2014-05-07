2a8a001b104b87be9a96ed17c74650541ff71066
yus <yus.projectproto@gmail.com>
2014-05-07 18:20:13 +0800

    working nanox screen driver

--- nanox/drivers/scr_win32.c
+++ nanox/drivers/scr_win32.c
@@ -21,11 +21,11 @@
 
 /* SCREEN_WIDTH, SCREEN_HEIGHT and MWPIXEL_FORMAT define window size*/
 #ifndef SCREEN_WIDTH
-#define SCREEN_WIDTH	800
+#define SCREEN_WIDTH	240
 #endif
 
 #ifndef SCREEN_HEIGHT
-#define SCREEN_HEIGHT	600
+#define SCREEN_HEIGHT	320
 #endif
 
 #ifndef MWPIXEL_FORMAT
@@ -66,10 +66,7 @@ static HANDLE dummyEvent = NULL;
 LRESULT
 myWindowProc(HWND hWnd,	UINT Msg, WPARAM wParam, LPARAM lParam)
 {
-	BOOL ret;
 	HDC dc;
-	PAINTSTRUCT ps;
-
 	switch (Msg) {
 	case WM_CREATE:
 		break;
@@ -95,13 +92,9 @@ myWindowProc(HWND hWnd,	UINT Msg, WPARAM wParam, LPARAM lParam)
 	case WM_SYSDEADCHAR:
 		break;
 	case WM_PAINT:
-		dc = BeginPaint(hWnd, &ps);
-		ret = BitBlt(dc, ps.rcPaint.left, ps.rcPaint.top, 
-			ps.rcPaint.right - ps.rcPaint.left,
-			ps.rcPaint.bottom - ps.rcPaint.top, 
-			dcBuffer, ps.rcPaint.left, ps.rcPaint.top, SRCCOPY);
-		assert(ret!=FALSE);
-		EndPaint(hWnd, &ps);
+		dc = GetDC(winRootWindow);
+		BitBlt(dc, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, dcBuffer, 0, 0, SRCCOPY);
+		ReleaseDC(winRootWindow, dc);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -114,7 +107,7 @@ myWindowProc(HWND hWnd,	UINT Msg, WPARAM wParam, LPARAM lParam)
 	default:
 		return DefWindowProc(hWnd, Msg, wParam, lParam);
 	}
-	return 0;	
+	return 0;
 }
  
 /* called from keyboard/mouse/screen */
@@ -122,13 +115,10 @@ static PSD
 win32_open(PSD psd)
 {
 	HANDLE hInstance = GetModuleHandle(NULL);
-	HDC rootDC = CreateDC("DISPLAY", NULL, NULL, NULL);
-	int depth = GetDeviceCaps(rootDC, BITSPIXEL);
 	RECT rect;
 	PSUBDRIVER subdriver;
 	WNDCLASS wc;
 
-	DeleteDC(rootDC);
 	GetWindowRect(GetDesktopWindow(), &rect);
 	psd->xvirtres = rect.right - rect.left;
 	psd->yvirtres = rect.bottom - rect.top;
@@ -214,280 +204,36 @@ win32_getscreeninfo(PSD psd, PMWSCREENINFO psi)
 	psi->fbdriver = FALSE;	/* not running fb driver, no direct map */
 }
 
-
-#if 0000
-static void
-win32_drawpixel(PSD psd, MWCOORD x, MWCOORD y, MWPIXELVAL pixel)
-{
-	COLORREF c = PIXELVALTOCOLORVAL(pixel);
-	HDC dc = GetDC(winRootWindow);
-
-	SetPixel(dc, x, y, c);
-	ReleaseDC(winRootWindow, dc);
-	SetPixel(dcBuffer, x, y, c);
-}
-
-static MWPIXELVAL
-win32_readpixel(PSD psd, MWCOORD x, MWCOORD y)
-{
-	COLORREF ret = GetPixel(dcBuffer, x, y);
-
-	return COLORVALTOPIXELVAL(ret);
-}
-
-static void
-drawLine(HDC dc, MWCOORD x1, MWCOORD y1, MWCOORD x2, MWCOORD y2, COLORREF c)
-{
-	POINT p;
-	HPEN pen = CreatePen(PS_SOLID, 1, c); 
-
-	if (pen) {
-		HGDIOBJ old = SelectObject(dc, pen);
-
-		MoveToEx(dc, x1, y1, &p);
-		LineTo(dc, x2, y2);
-		SelectObject(dc, old);
-		DeleteObject(pen);
-	}
-}
-
-static void
-win32_drawhline(PSD psd, MWCOORD x1, MWCOORD x2, MWCOORD y, MWPIXELVAL pixel)
-{
-	COLORREF c = PIXELVALTOCOLORVAL(pixel);
-	HDC dc = GetDC(winRootWindow);
-
-	drawLine(dc, x1, y, x2, y, c);
-	ReleaseDC(winRootWindow, dc);
-	drawLine(dcBuffer, x1, y, x2, y, c);
-}
-
-static void
-win32_drawvline(PSD psd, MWCOORD x, MWCOORD y1, MWCOORD y2, MWPIXELVAL pixel)
-{
-	COLORREF c = PIXELVALTOCOLORVAL(pixel);
-	HDC dc = GetDC(winRootWindow);
-
-	drawLine(dc, x, y1, x, y2, c);
-	ReleaseDC(winRootWindow, dc);
-	drawLine(dcBuffer, x, y1, x, y2, c);
-}
-
-static void
-win32_fillrect(PSD psd, MWCOORD x1, MWCOORD y1, MWCOORD x2, MWCOORD y2,
-	MWPIXELVAL pixel)
-{
-	RECT rect;
-	HDC dc;
-	HBRUSH hbr = CreateSolidBrush(PIXELVALTOCOLORVAL(pixel));
-
-	if (hbr) {
-		rect.bottom = y2;
-		rect.top = y1;
-		rect.left = x1;
-		rect.right = x2;
-		dc = GetDC(winRootWindow);
-		FillRect(dc, &rect, hbr);
-		ReleaseDC(winRootWindow, dc);
-		FillRect(dcBuffer, &rect, hbr);
-		DeleteObject(hbr);
-	}
-}
-
-static void
-win32_blit(PSD dstpsd, MWCOORD destx, MWCOORD desty, MWCOORD w, MWCOORD h,
-	 PSD srcpsd, MWCOORD srcx, MWCOORD srcy, long op)
-{
-	unsigned char *addr = srcpsd->addr;
-	HDC dc;
-
-	if (op == MWROP_NOOP)
-		return;
-
-	if (!(dstpsd->flags & PSF_SCREEN)) {
-		/* memory to memory blit, use offscreen blitter */
-		dstpsd->Blit(dstpsd, destx, desty, w, h,
-			srcpsd, srcx, srcy, op);
-		return;
-	}
- 
-	dc = GetDC(winRootWindow);
-	if (srcpsd->flags & PSF_MEMORY) {
-            unsigned int i;
-            unsigned char *bits = NULL;
-            BITMAPV4HEADER bmpInfo;
-
-            memset(&bmpInfo, 0, sizeof(bmpInfo));
-            bmpInfo.bV4Size = sizeof(bmpInfo);
-            bmpInfo.bV4Width = srcpsd->xres;
-            bmpInfo.bV4Height = -srcpsd->yres; /* top-down image */
-            bmpInfo.bV4Planes = 1;
-            bmpInfo.bV4BitCount = srcpsd->bpp;
-            bmpInfo.bV4SizeImage = srcpsd->size;
-        
-        switch (srcpsd->pixtype) {
-        case MWPF_TRUECOLOR565:
-            bmpInfo.bV4RedMask  = 0xf800u;
-            bmpInfo.bV4GreenMask= 0x07e0u;
-            bmpInfo.bV4BlueMask = 0x001fu;
-            bmpInfo.bV4V4Compression = BI_BITFIELDS;
-            break;
-        case MWPF_TRUECOLOR555:
-            bmpInfo.bV4RedMask  = 0x7c00u;
-            bmpInfo.bV4GreenMask= 0x03e0u;
-            bmpInfo.bV4BlueMask = 0x001fu;
-            bmpInfo.bV4V4Compression = BI_BITFIELDS;
-            break;
-        case MWPF_TRUECOLOR888:
-            if ((bits = (unsigned char *)malloc(bmpInfo.bV4SizeImage)) != NULL) {
-                for (i=0; i<bmpInfo.bV4SizeImage; i+=3) {
-                    bits[i] = addr[i+2];
-                    bits[i+1] = addr[i+1];
-                    bits[i+2] = addr[i];
-                }
-                addr = bits;
-            }
-            bmpInfo.bV4V4Compression = BI_RGB;
-            break;
-        case MWPF_TRUECOLORABGR:
-            bmpInfo.bV4AlphaMask = 0xff000000;
-            bmpInfo.bV4RedMask = 0x0000ff;
-            bmpInfo.bV4GreenMask= 0x00ff00;
-            bmpInfo.bV4BlueMask  = 0xff0000;
-            bmpInfo.bV4V4Compression = BI_BITFIELDS;
-			break;
-        case MWPF_TRUECOLOR8888:
-            bmpInfo.bV4AlphaMask = 0xff000000;
-            bmpInfo.bV4RedMask  = 0xff0000;
-            bmpInfo.bV4GreenMask= 0x00ff00;
-            bmpInfo.bV4BlueMask = 0x0000ff;
-            bmpInfo.bV4V4Compression = BI_BITFIELDS;
-            break;
-        }
-        
-        bmpInfo.bV4XPelsPerMeter = 3078;
-        bmpInfo.bV4YPelsPerMeter = 3078;
-        bmpInfo.bV4ClrUsed = 0;
-        bmpInfo.bV4ClrImportant = 0;   
-        bmpInfo.bV4CSType = LCS_sRGB;
-        srcy = srcpsd->yres - h - srcy;
-#if 1
-        SetDIBitsToDevice(dc, destx, desty, w, h, srcx, srcy, 0, srcpsd->yres,
-		addr, (BITMAPINFO*)&bmpInfo, DIB_RGB_COLORS);
-        SetDIBitsToDevice(dcBuffer, destx, desty, w, h, srcx, srcy, 0, srcpsd->yres,
-		addr, (BITMAPINFO*)&bmpInfo, DIB_RGB_COLORS);
-#else
-        StretchDIBits(dc, destx, desty, w, h, srcx, srcy, w, h,
-		addr, &bmpInfo, DIB_RGB_COLORS, SRCCOPY);
-        StretchDIBits(dcBuffer, destx, desty, w, h, srcx, srcy, w, h,
-		addr, &bmpInfo, DIB_RGB_COLORS, SRCCOPY);
-#endif 
-        free(bits);
-	} else if (srcpsd->flags & PSF_SCREEN) {
-		/* Use offscreen equivalent as the source */
-		BitBlt(dc, destx, desty, w, h, dcBuffer, srcx, srcy, SRCCOPY);
-		BitBlt(dcBuffer, destx, desty, w, h, dcBuffer, srcx, srcy, SRCCOPY);
-	}
-	ReleaseDC(winRootWindow, dc);
-}
-#endif
-
-static void
-update_from_savebits(PSD psd, int destx, int desty, int w, int h)
-{
-#if 0
-	XImage *img;
-	unsigned int x, y;
-	char *data;
-
-	/* allocate buffer */
-	if (x11_depth >= 24)
-		data = malloc(w * 4 * h);
-	else if (x11_depth > 8)	/* 15, 16 */
-		data = malloc(w * 2 * h);
-	else			/* 1,2,4,8 */
-		data = malloc((w * x11_depth + 7) / 8 * h);
-
-	/* copy from offscreen to screen */
-	img = XCreateImage(x11_dpy, x11_vis, x11_depth, ZPixmap, 0, data, w, h, 8, 0);
-
-	/* Use optimized loops for most common framebuffer modes */
-
-#if MWPIXEL_FORMAT == MWPF_TRUECOLOR332
-	{
-		unsigned char *addr = psd->addr + desty * psd->pitch + destx;
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				MWPIXELVAL c = addr[x];
-				unsigned long pixel = PIXELVAL_to_pixel(c);
-				XPutPixel(img, x, y, pixel);
-			}
-			addr += psd->pitch;
-		}
-	}
-#elif (MWPIXEL_FORMAT == MWPF_TRUECOLOR565) || (MWPIXEL_FORMAT == MWPF_TRUECOLOR555)
-	{
-		unsigned char *addr = psd->addr + desty * psd->pitch + (destx << 1);
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				MWPIXELVAL c = ((ADDR16)addr)[x];
-				unsigned long pixel = PIXELVAL_to_pixel(c);
-				XPutPixel(img, x, y, pixel);
-			}
-			addr += psd->pitch;
-		}
-	}
-#elif MWPIXEL_FORMAT == MWPF_TRUECOLOR888
-	{
-		unsigned char *addr = psd->addr + desty * psd->pitch + destx * 3;
-		unsigned int extra = psd->pitch - w * 3;
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				MWPIXELVAL c = RGB2PIXEL888(addr[2], addr[1], addr[0]);
-				unsigned long pixel = PIXELVAL_to_pixel(c);
-				XPutPixel(img, x, y, pixel);
-				addr += 3;
-			}
-			addr += extra;
-		}
-	}
-#elif (MWPIXEL_FORMAT == MWPF_TRUECOLOR8888) || (MWPIXEL_FORMAT == MWPF_TRUECOLORABGR)
-	{
-		unsigned char *addr = psd->addr + desty * psd->pitch + (destx << 2);
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				MWPIXELVAL c = ((ADDR32)addr)[x];
-				unsigned long pixel = PIXELVAL_to_pixel(c);
-				XPutPixel(img, x, y, pixel);
-			}
-			addr += psd->pitch;
-		}
-	}
-#else /* MWPF_PALETTE*/
-	{
-		unsigned char *addr = psd->addr + desty * psd->pitch + destx;
-		for (y = 0; y < h; y++) {
-			for (x = 0; x < w; x++) {
-				MWPIXELVAL c = addr[x];
-				unsigned long pixel = PIXELVAL_to_pixel(c);
-				XPutPixel(img, x, y, pixel);
-			}
-			addr += psd->pitch;
-		}
-	}
-#endif
-
-	XPutImage(x11_dpy, x11_win, x11_gc, img, 0, 0, destx, desty, w, h);
-	XDestroyImage(img);
-#endif
-}
-
 static void
 win32_update(PSD psd, MWCOORD x, MWCOORD y, MWCOORD width, MWCOORD height)
 {
-	if (!width)
-		width = psd->xres;
-	if (!height)
-		height = psd->yres;
-	update_from_savebits(psd, x, y, width, height);
+	// full area redraw
+	unsigned char *addr = psd->addr;
+	BITMAPV4HEADER bmpInfo;
+	
+	if(psd->pixtype != MWPF_TRUECOLOR8888)
+		return; // todo: other pixtypes
+	
+	memset(&bmpInfo, 0, sizeof(bmpInfo));
+	bmpInfo.bV4Size = sizeof(bmpInfo);
+	bmpInfo.bV4Width = psd->xres;
+	bmpInfo.bV4Height = -psd->yres; /* top-down image */
+	bmpInfo.bV4Planes = 1;
+	bmpInfo.bV4BitCount = psd->bpp;
+	bmpInfo.bV4SizeImage = psd->size;
+        
+	bmpInfo.bV4AlphaMask = 0xff000000;
+	bmpInfo.bV4RedMask  = 0xff0000;
+	bmpInfo.bV4GreenMask= 0x00ff00;
+	bmpInfo.bV4BlueMask = 0x0000ff;
+	bmpInfo.bV4V4Compression = BI_BITFIELDS;
+
+	bmpInfo.bV4XPelsPerMeter = 3078;
+	bmpInfo.bV4YPelsPerMeter = 3078;
+	bmpInfo.bV4ClrUsed = 0;
+	bmpInfo.bV4ClrImportant = 0;   
+	bmpInfo.bV4CSType = LCS_CALIBRATED_RGB;
+	
+	SetDIBitsToDevice(dcBuffer, 0, 0, psd->xres, psd->yres, 0, 0, 0, psd->yres,
+			addr, (BITMAPINFO*)&bmpInfo, DIB_RGB_COLORS);
 }
